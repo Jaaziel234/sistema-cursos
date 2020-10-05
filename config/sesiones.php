bd5160fc6fb3c5cddb267da0a4be62b1ab88e456
José Deodanes-code <60304928+Jose-developer-start@users.noreply.github.com>
2020-10-05 09:37:19 -0600

    Add files via upload

---
+++ config/sesiones.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+//Si no existe esta session redirigime a index.php
+if (!isset($_SESSION['admin']) && !isset($_SESSION['adminPrincipal'])) {
+	header("Location:index.php"); 
+}
+?>
