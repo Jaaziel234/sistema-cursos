98a3a0a68ed10c94b73d3cbb2614a219c42606e5
Jaaziel234 <63311578+Jaaziel234@users.noreply.github.com>
2020-10-08 10:11:56 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,234 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title></title>
+
+     <title>sitio en construccion</title>
+  
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="team" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+     <link rel="stylesheet" href="recursos/css/bootstrap.min.css">
+     <link rel="stylesheet" href="recursos/css/owl.theme.default.min.css">
+     <link rel="stylesheet" href="recursos/css/font-awesome.min.css">
+
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="recursos/css/tooplate-style.css">
+
 </head>
 <body>
+     <!-- MENU -->
+     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- lOGO ACA -->
+                    <a href="index.html" class="navbar-brand">CODERZOOM</a>
+               </div>
+
+               <!-- MENU LINKS -->
+               <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                          <li><a class="nav-link" href="cursos.php">Cursos</a></li>
+                          <li><a class="nav-link" href="pago.php">Premium</a></li>
+                         <li><a class="nav-link" href="registrarse.php">REGISTRARSE</a></li>
+                         <li><a class="nav-link" href="loginU.php">Iniciar sesion</a></li>
+                        
+                    </ul>
+                    
+               </div>
+
+          </div>
+     </section>
+
+
+     <!-- FEATURE -->
+     <section id="home">
+          <div class="overlay"></div>
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-offset-3 col-md-6 col-sm-12">
+                         <div class="home-info">
+                              <h3>BIENVENIDO</h3>
+                              <h1>La plataforma de apredizaje para desarrolladores!</h1>
+                         </div>
+                         
+                    </div>
+               </div>
+          </div>
+     </section>
+
+     <!-- ABOUT -->
+     <section id="about">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-offset-3 col-md-6 col-sm-12">
+                         <div class="section-title">
+                              <h1>Así funciona CODERZOOM </h1>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="team-thumb">
+                              <img src="recursos/images/persona_1.jpg" class="img-responsive" alt="Andrew Orange">
+                              <div class="team-info team-thumb-up">
+                                   <h2>Andrew Orange</h2>
+                                   <small>Art Director</small>
+                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</p>
+                              </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="team-thumb">
+                              <div class="team-info team-thumb-down">
+                                   <h2>Catherine Soft</h2>
+                                   <small>Senior Manager</small>
+                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</p>
+                              </div>
+                              <img src="recursos/images/persona_2.jpg" class="img-responsive" alt="Catherine Soft">
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4">
+                         <div class="team-thumb">
+                              <img src="recursos/images/persona_3.jpg" class="img-responsive" alt="Jack Wilson">
+                              <div class="team-info team-thumb-up">
+                                   <h2>Jack Wilson</h2>
+                                   <small>CEO / Founder</small>
+                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</p>
+                              </div>
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </section>
+
+
+    <!-- info contenido 2 -->
+     <section id="ver">
+          <div class="container">
+               <div class="row">
+
+                    <div class="col-md-12 col-sm-12">
+                         <div class="section-title">
+                              <h1>Conoce más de nuestros contenidos</h1>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6">
+                         <div class="ver-thumb">
+                             <div class="ver-title">
+                                  <h2>Estudia a tu ritmo</h2>
+                             </div>
+                             <div class="ver-info">
+                                   <p>Programar no tiene que ser tan dificil. Aprende los fundamentos desde cero y obtén una base sólida.</p>
+                                   
+                             </div>
+                             <div class="ver-bottom">
+                                    <span class="pricing-dollar">A delante!</span>
+                                   <a href="#" class="section-btn ver-btn">Registarse ahora</a>
+                             </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6">
+                         <div class="ver-thumb">
+                             <div class="ver-title">
+                                  <h2>Estudia a tu ritmo</h2>
+                             </div>
+                             <div class="ver-info">
+                                   <p>Programar no tiene que ser tan dificil. Aprende los fundamentos desde cero y obtén una base sólida.</p>
+                             </div>
+                             <div class="ver-bottom">
+                                    <span class="pricing-dollar">Listo!</span>
+                                   <a href="#" class="section-btn ver-btn">Registrarse ahora</a>
+                             </div>
+                         </div>
+                    </div>
+
+                    <div class="col-md-4 col-sm-6">
+                         <div class="ver-thumb">
+                             <div class="ver-title">
+                                  <h2>Estudia a tu ritmo</h2>
+                             </div>
+                             <div class="ver-info">
+                                   <p>Programar no tiene que ser tan dificil. Aprende los fundamentos desde cero y obtén una base sólida.</p>
+                             </div>
+                             <div class="ver-bottom">
+                                    <span class="pricing-dollar">Vamos!</span>
+                                   <a href="#" class="section-btn ver-btn">Registrase ahora</a>
+                             </div>
+                         </div>
+                    </div>
+                    
+               </div>
+          </div>
+     </section>   
+
+
+<footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-info">
+            <h3>CODERZOOM</h3>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Links</h4>
+            <ul>
+              <li><a href="#">Cursos</a></li>
+              <li><a href="#">Premium</a></li>
+              <li><a href="loginU.php">Iniciar sesion</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contactanos</h4>
+            <p>
+              <strong>Telefono:</strong> +503 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
+            
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
+</footer><!-- #footer -->
+
+     <!-- SCRIPTS -->
+     <script src="recursos/js/jquery.js"></script>
+     <script src="recursos/js/custom.js"></script>
 
 </body>
 </html>
