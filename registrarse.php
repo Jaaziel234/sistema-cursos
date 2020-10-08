8445085ca4e1cb3d93be203d6c9da3fea175c3e6
Jaaziel234 <63311578+Jaaziel234@users.noreply.github.com>
2020-10-07 21:35:56 -0600

    Add files via upload

---
+++ registrarse.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+     <title>login</title>
+    
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="description" content="">
+     <meta name="keywords" content="">
+     <meta name="team" content="">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <link rel="stylesheet" href="css/bootstrap.min.css">
+     <link rel="stylesheet" href="css/owl.theme.default.min.css">
+     <link rel="stylesheet" href="css/font-awesome.min.css">
+
+     <!-- Font-->
+     <link rel="stylesheet" type="text/css" href="css/opensans-font.css">
+     <link rel="stylesheet" type="text/css" href="fonts/line-awesome/css/line-awesome.min.css">
+     <!-- Jquery -->
+     <link rel="stylesheet" href="https://jqueryvalidation.org/files/demo/site-demos.css">
+     <!-- Main Style Css -->
+     <link rel="stylesheet" href="css/style.css"/>
+      
+     <!-- MAIN CSS -->
+     <link rel="stylesheet" href="css/tooplate-style.css">
+
+</head>
+<body class="form-v4">
+  <div class="page-content">
+    <div class="form-v4-content">
+      <div class="form-left">
+        <h2>INFOMATION</h2>
+        <p class="text-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et molestie ac feugiat sed. Diam volutpat commodo.</p>
+        <p class="text-2"><span>Eu ultrices:</span> Vitae auctor eu augue ut. Malesuada nunc vel risus commodo viverra. Praesent elementum facilisis leo vel.</p>
+        <div class="form-left-last">
+          <input type="submit" name="account" class="account" value="Have An Account">
+        </div>
+      </div>
+      <form class="form-detail" action="#" method="post" id="myform">
+        <h2>REGISTER FORM</h2>
+        <div class="form-group">
+          <div class="form-row form-row-1">
+            <label for="first_name">First Name</label>
+            <input type="text" name="first_name" id="first_name" class="input-text">
+          </div>
+          <div class="form-row form-row-1">
+            <label for="last_name">Last Name</label>
+            <input type="text" name="last_name" id="last_name" class="input-text">
+          </div>
+        </div>
+        <div class="form-row">
+          <label for="your_email">Your Email</label>
+          <input type="text" name="your_email" id="your_email" class="input-text" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
+        </div>
+        <div class="form-group">
+          <div class="form-row form-row-1 ">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" class="input-text" required>
+          </div>
+          <div class="form-row form-row-1">
+            <label for="comfirm-password">Comfirm Password</label>
+            <input type="password" name="comfirm_password" id="comfirm_password" class="input-text" required>
+          </div>
+        </div>
+        <div class="form-checkbox">
+          <label class="container"><p>I agree to the <a href="#" class="text">Terms and Conditions</a></p>
+              <input type="checkbox" name="checkbox">
+              <span class="checkmark"></span>
+          </label>
+        </div>
+        <div class="form-row-last">
+          <input type="submit" name="register" class="register" value="Register">
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-info">
+            <h3>CODIGOZOOM</h3>
+            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Terms of service</a></li>
+              <li><a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              A108 Adam Street <br>
+              New York, NY 535022<br>
+              United States <br>
+              <strong>Phone:</strong> +1 5589 55488 55<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit"  value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
+
+     <!-- MENU -->
+     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
+          <div class="container">
+
+               <div class="navbar-header">
+                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                         <span class="icon icon-bar"></span>
+                    </button>
+
+                    <!-- lOGO TEXT HERE -->
+                    <a class="navbar-brand2">CODERZOOM</a>
+               </div>
+
+          </div>
+     </section>
+ 
+
+  </footer><!-- #footer -->
+
+     <!-- SCRIPTS -->
+     <script src="js/jquery.js"></script>
+     <script src="js/bootstrap.min.js"></script>
+     <script src="js/jquery.stellar.min.js"></script>
+     <script src="js/smoothscroll.js"></script>
+     <script src="js/custom.js"></script>
+
+     <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
+    
+</body>
+</html>
