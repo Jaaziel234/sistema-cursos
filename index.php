612d96d2ba1e8116fe0310a417d9688c4370e4da
Jaaziel234 <63311578+Jaaziel234@users.noreply.github.com>
2020-10-09 09:31:41 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,137 +1,103 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+<!doctype html>
+<html lang="en-US">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-     <title>sitio en construccion</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="recursos/css/style.css" type="text/css" />
+
+    <!-- Ionic icons -->
+    <link href="https://unpkg.com/ionicons@4.2.0/dist/css/ionicons.min.css" rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900" rel="stylesheet">
+
+    <title>SITIO EN CONSTRUCCION</title>
+  </head>
+
+  <body>
+
+  <!-- N A V B A R -->
+  <nav class="navbar navbar-default navbar-expand-lg fixed-top custom-navbar">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="icon ion-md-menu"></span>
+    </button>
+    <img src="recursos/images/logor.png" class="img-fluid nav-logo-mobile" alt="Company Logo">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <div class="container">
+      	<img src="recursos/images/logor.png" class="img-fluid nav-logo-desktop" alt="Company Logo">
+        <ul class="navbar-nav ml-auto nav-right" data-easing="easeInOutExpo" data-speed="1250" data-offset="65">
+          <li class="nav-item nav-custom-link">
+            <a class="nav-link" href="index.html">CURSOS <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+          </li>
+          <li class="nav-item nav-custom-link">
+            <a class="nav-link" href=" ">PREMIUM <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+          </li>
+          <li class="nav-item nav-custom-link">
+            <a class="nav-link" href="iniciarSesion.php">INICIAR SESION <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+          </li>
+          <li class="nav-item nav-custom-link btn btn-demo-small">
+            <a class="nav-link" href="registrarse.php">REGISTRARSE <i class="icon ion-ios-arrow-forward icon-mobile"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- E N D  N A V B A R -->
   
-     <meta charset="UTF-8">
-     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-     <meta name="description" content="">
-     <meta name="keywords" content="">
-     <meta name="team" content="">
-     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <!-- H E R O -->
+  <section id="hero">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5">
+          <img src="recursos/images/persona.jpg" class="img-fluid" alt="Demo image">
+        </div>
+        <div class="col-md-7 content-box hero-content text-center">
+          <span>BIENVENIDOS</span>
+          <h1>CODERZOOM</b></h1>
+          <p>La plataforma de apredizaje para desarrolladores</p>
+          <a href="#" class="btn btn-regular">Leer mas</a>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- E N D  H E R O -->
 
-     <link rel="stylesheet" href="recursos/css/bootstrap.min.css">
-     <link rel="stylesheet" href="recursos/css/owl.theme.default.min.css">
-     <link rel="stylesheet" href="recursos/css/font-awesome.min.css">
-
-     <!-- MAIN CSS -->
-     <link rel="stylesheet" href="recursos/css/tooplate-style.css">
-
-</head>
-<body>
-     <!-- MENU -->
-     <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
-          <div class="container">
-
-               <div class="navbar-header">
-                    <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                         <span class="icon icon-bar"></span>
-                    </button>
-
-                    <!-- lOGO ACA -->
-                    <a href="index.html" class="navbar-brand">CODERZOOM</a>
-               </div>
-
-               <!-- MENU LINKS -->
-               <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
-                          <li><a class="nav-link" href="cursos.php">Cursos</a></li>
-                          <li><a class="nav-link" href="pago.php">Premium</a></li>
-                         <li><a class="nav-link" href="registrarse.php">REGISTRARSE</a></li>
-                         <li><a class="nav-link" href="loginU.php">Iniciar sesion</a></li>
-                        
-                    </ul>
-                    
-               </div>
-
+  <!--  M A R K E T I N G -->
+  <section id="marketing">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5">
+          <div class="content-box">
+            <h2>APRENDE DESDE CERO</h2>
+            <p>Programar no tiene que ser tan dificil. Aprende los fundamentos desde cero y obtén una base sólida.</p>
+            <a href="#" class="btn btn-regular">COMENZAR</a>
           </div>
-     </section>
+        </div>
+        <div class="col-md-7">
+            <img src="recursos/images/fondo1.jpg" class="img-fluid" alt="Demo image">
+        </div>
+      </div>
+    </div>
+  </section>
 
-
-     <!-- FEATURE -->
-     <section id="home">
-          <div class="overlay"></div>
+  <section id="ver">
           <div class="container">
                <div class="row">
-
-                    <div class="col-md-offset-3 col-md-6 col-sm-12">
-                         <div class="home-info">
-                              <h3>BIENVENIDO</h3>
-                              <h1>La plataforma de apredizaje para desarrolladores!</h1>
-                         </div>
-                         
-                    </div>
-               </div>
-          </div>
-     </section>
-
-     <!-- ABOUT -->
-     <section id="about">
-          <div class="container">
-               <div class="row">
-
-                    <div class="col-md-offset-3 col-md-6 col-sm-12">
-                         <div class="section-title">
-                              <h1>Así funciona CODERZOOM </h1>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-4">
-                         <div class="team-thumb">
-                              <img src="recursos/images/persona_1.jpg" class="img-responsive" alt="Andrew Orange">
-                              <div class="team-info team-thumb-up">
-                                   <h2>Andrew Orange</h2>
-                                   <small>Art Director</small>
-                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</p>
-                              </div>
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-4">
-                         <div class="team-thumb">
-                              <div class="team-info team-thumb-down">
-                                   <h2>Catherine Soft</h2>
-                                   <small>Senior Manager</small>
-                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</p>
-                              </div>
-                              <img src="recursos/images/persona_2.jpg" class="img-responsive" alt="Catherine Soft">
-                         </div>
-                    </div>
-
-                    <div class="col-md-4 col-sm-4">
-                         <div class="team-thumb">
-                              <img src="recursos/images/persona_3.jpg" class="img-responsive" alt="Jack Wilson">
-                              <div class="team-info team-thumb-up">
-                                   <h2>Jack Wilson</h2>
-                                   <small>CEO / Founder</small>
-                                   <p>Lorem ipsum dolor sit amet, consectetur adipisicing eiusmod tempor incididunt ut labore et dolore magna.</p>
-                              </div>
-                         </div>
-                    </div>
-                    
-               </div>
-          </div>
-     </section>
-
-
-    <!-- info contenido 2 -->
-     <section id="ver">
-          <div class="container">
-               <div class="row">
-
                     <div class="col-md-12 col-sm-12">
                          <div class="section-title">
                               <h1>Conoce más de nuestros contenidos</h1>
                          </div>
                     </div>
-
                     <div class="col-md-4 col-sm-6">
                          <div class="ver-thumb">
                              <div class="ver-title">
-                                  <h2>Estudia a tu ritmo</h2>
+                                  <h3>Estudia a tu ritmo</h3>
                              </div>
                              <div class="ver-info">
                                    <p>Programar no tiene que ser tan dificil. Aprende los fundamentos desde cero y obtén una base sólida.</p>
@@ -139,37 +105,35 @@
                              </div>
                              <div class="ver-bottom">
                                     <span class="pricing-dollar">A delante!</span>
-                                   <a href="#" class="section-btn ver-btn">Registarse ahora</a>
+                                   <a href="#" class="section-btn ver-btn">Register now</a>
                              </div>
                          </div>
                     </div>
-
                     <div class="col-md-4 col-sm-6">
                          <div class="ver-thumb">
                              <div class="ver-title">
-                                  <h2>Estudia a tu ritmo</h2>
+                                  <h3>Estudia a tu ritmo</h3>
                              </div>
                              <div class="ver-info">
                                    <p>Programar no tiene que ser tan dificil. Aprende los fundamentos desde cero y obtén una base sólida.</p>
                              </div>
                              <div class="ver-bottom">
                                     <span class="pricing-dollar">Listo!</span>
-                                   <a href="#" class="section-btn ver-btn">Registrarse ahora</a>
+                                   <a href="#" class="section-btn ver-btn">Register now</a>
                              </div>
                          </div>
                     </div>
-
                     <div class="col-md-4 col-sm-6">
                          <div class="ver-thumb">
                              <div class="ver-title">
-                                  <h2>Estudia a tu ritmo</h2>
+                                  <h3>Estudia a tu ritmo</h3>
                              </div>
                              <div class="ver-info">
                                    <p>Programar no tiene que ser tan dificil. Aprende los fundamentos desde cero y obtén una base sólida.</p>
                              </div>
                              <div class="ver-bottom">
                                     <span class="pricing-dollar">Vamos!</span>
-                                   <a href="#" class="section-btn ver-btn">Registrase ahora</a>
+                                   <a href="#" class="section-btn ver-btn">Register now</a>
                              </div>
                          </div>
                     </div>
@@ -178,57 +142,113 @@
           </div>
      </section>   
 
-
-<footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6 footer-info">
-            <h3>CODERZOOM</h3>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Links</h4>
-            <ul>
-              <li><a href="#">Cursos</a></li>
-              <li><a href="#">Premium</a></li>
-              <li><a href="loginU.php">Iniciar sesion</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h4>Contactanos</h4>
-            <p>
-              <strong>Telefono:</strong> +503 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-
-            <div class="social-links">
-              <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-              <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-              <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-              
-            </div>
-
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem marada parida nodela caramase seza.</p>
-            
-          </div>
-
-        </div>
+  <!-- C A L L  T O  A C T I O N -->
+  <section id="call-to-action">
+    <div class="container text-center">
+      <h2>Conoce más de nuestros contenidos</h2>
+      <div class="title-block">
+        <a href="#" class="btn btn-regular">Explorar Más</a>
       </div>
     </div>
+  </section>
+
+<div class="container"></div>
+      <!--targetas-->
+      <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
+          <div class="card mb-4  mt-4 shadow-lg mt-4" style="width: 18rem;">
+            <img src="recursos/images/tarjeta1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
+          <div class="card mb-4  mt-4 shadow-lg mt-4" style="width: 18rem;">
+            <img src="recursos/images/tarjeta1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
+          <div class="card mb-4  mt-4 shadow-lg mt-4" style="width: 18rem;">
+            <img src="recursos/images/tarjeta1.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      </div> <!--final targetas-->
+
+  <!--  F O O T E R  -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h5>Seo Ranking</h5>
+          <ul>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Affiliate Program</a></li>
+            <li><a href="#">Developer API</a></li>
+            <li><a href="#">Support</a></li>
+            <li><a href="#">Video Tutorials</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Main Tools</h5>
+          <ul>
+            <li><a href="#">Rank Tracker</a></li>
+            <li><a href="#">Backlink Checker</a></li>
+            <li><a href="#">Keyword Generator</a></li>
+            <li><a href="#">Serp Checker</a></li>
+            <li><a href="#">Site Audit</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Blog</h5>
+          <ul>
+            <li><a href="#">Get High Quality Backlinks</a></li>
+            <li><a href="#">Top Google Searches</a></li>
+            <li><a href="#">Avoid Google Penalties</a></li>
+            <li><a href="#">White Hat SEO Tips</a></li>
+            <li><a href="#">Google Trends</a></li>
+          </ul>
+        </div>
+        <div class="col-md-3">
+          <h5>Company</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur scelerisque, tortor nec mattis feugiat, velit purus euismod odio, quis vulputate velit urna.</p>
+          <p><a href="coderzoom@company.com" class="external-links">coderzoom@company.com</a></p>
+        </div>
+      </div> 
+      <div class="divider"></div>
+      <div class="row">
+        <div class="col-md-6 col-xs-12">
+            <a href="#"><i class="icon ion-logo-facebook"></i></a>
+            <a href="#"><i class="icon ion-logo-instagram"></i></a>
+            <a href="#"><i class="icon ion-logo-twitter"></i></a>
+            <a href="#"><i class="icon ion-logo-youtube"></i></a>
+          </div>
+          <div class="col-md-6 col-xs-12">
+            <small>2020 &copy; All rights reserved. Made by <a href=" " target="blank" class="external-links">CODERZOOM</a></small>
+          </div>
+      </div>
     </div>
-</footer><!-- #footer -->
-
-     <!-- SCRIPTS -->
-     <script src="recursos/js/jquery.js"></script>
-     <script src="recursos/js/custom.js"></script>
-
-</body>
+  </footer>
+  <!--  E N D  F O O T E R  -->
+    
+    <!-- External JavaScripts -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  </body>
 </html>
