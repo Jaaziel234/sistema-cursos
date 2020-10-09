3d4cf88dc8c43fef1daae89f4173cc6514e6a610
José Deodanes-code <60304928+Jose-developer-start@users.noreply.github.com>
2020-10-09 10:07:19 -0600

    Add files via upload

---
+++ sistema-cursos.sql
@@ -0,0 +1,409 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-10-2020 a las 18:06:48
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.2.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistema-cursos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrador`
+--
+
+CREATE TABLE `administrador` (
+  `Id` int(11) NOT NULL,
+  `Nombres` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Apellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Contraseña` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Estado` tinyint(2) UNSIGNED NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `administrador`
+--
+
+INSERT INTO `administrador` (`Id`, `Nombres`, `Apellidos`, `Usuario`, `Contraseña`, `Estado`) VALUES
+(1, 'Jose', 'Deodanes', 'admin1', 'admin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrera`
+--
+
+CREATE TABLE `carrera` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `carrera`
+--
+
+INSERT INTO `carrera` (`Id`, `Nombre`) VALUES
+(1, 'Computacion 1'),
+(8, 'Ingeniería mecanica'),
+(9, 'Licenciatura en computacion'),
+(10, 'Economia');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentario`
+--
+
+CREATE TABLE `comentario` (
+  `Id` int(11) NOT NULL,
+  `Fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Id_Usuario` int(11) NOT NULL,
+  `Id_Curso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `contenido`
+--
+
+CREATE TABLE `contenido` (
+  `Id` int(11) NOT NULL,
+  `Temas` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `DuracionVideo` decimal(50,0) NOT NULL,
+  `Video` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `Id_Curso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `curso`
+--
+
+CREATE TABLE `curso` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Descripcion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `Fecha` date DEFAULT current_timestamp(),
+  `DuracionCurso` int(11) NOT NULL,
+  `Precio` decimal(11,0) NOT NULL,
+  `Imagen` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Id_Docente` int(11) NOT NULL,
+  `Id_Carrera` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente`
+--
+
+CREATE TABLE `docente` (
+  `Id` int(11) NOT NULL,
+  `Nombres` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Apellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Sexo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Contraseña` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Foto` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `Estado` tinyint(2) UNSIGNED NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pago`
+--
+
+CREATE TABLE `pago` (
+  `Id` int(11) NOT NULL,
+  `Nombre` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `pago`
+--
+
+INSERT INTO `pago` (`Id`, `Nombre`) VALUES
+(1, 'Paypal');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `plan`
+--
+
+CREATE TABLE `plan` (
+  `Id` int(11) NOT NULL,
+  `Tipo_Plan` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Descripcion` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Precio` decimal(20,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `plan`
+--
+
+INSERT INTO `plan` (`Id`, `Tipo_Plan`, `Descripcion`, `Precio`) VALUES
+(1, 'Normal', 'Contiene caracteristicas limitadas', '0.00');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tema_contenido`
+--
+
+CREATE TABLE `tema_contenido` (
+  `Id` int(11) NOT NULL,
+  `Tema` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `Id_Curso` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `Id` int(11) NOT NULL,
+  `Nombres` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Apellidos` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Sexo` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `Edad` tinyint(4) NOT NULL,
+  `Correo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Usuario` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Contraseña` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Foto` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `Id_Curso` int(11) NOT NULL,
+  `Id_Plan` int(11) NOT NULL,
+  `Id_Pago` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventacurso`
+--
+
+CREATE TABLE `ventacurso` (
+  `Id` int(11) NOT NULL,
+  `Correo` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `ClaveTransaccion` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `PaypalDato` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
+  `Fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Estado` varchar(20) COLLATE utf8_spanish_ci NOT NULL,
+  `Id_Usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `carrera`
+--
+ALTER TABLE `carrera`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_Usuario` (`Id_Usuario`),
+  ADD KEY `Id_Curso` (`Id_Curso`);
+
+--
+-- Indices de la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_Curso` (`Id_Curso`);
+
+--
+-- Indices de la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_Docente` (`Id_Docente`),
+  ADD KEY `Id_Carrera` (`Id_Carrera`);
+
+--
+-- Indices de la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `pago`
+--
+ALTER TABLE `pago`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `plan`
+--
+ALTER TABLE `plan`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Indices de la tabla `tema_contenido`
+--
+ALTER TABLE `tema_contenido`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_Contenido` (`Id_Curso`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_Curso` (`Id_Curso`),
+  ADD KEY `Id_Plan` (`Id_Plan`),
+  ADD KEY `Id_Pago` (`Id_Pago`);
+
+--
+-- Indices de la tabla `ventacurso`
+--
+ALTER TABLE `ventacurso`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_Usuario` (`Id_Usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `carrera`
+--
+ALTER TABLE `carrera`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+
+--
+-- AUTO_INCREMENT de la tabla `curso`
+--
+ALTER TABLE `curso`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT de la tabla `docente`
+--
+ALTER TABLE `docente`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT de la tabla `plan`
+--
+ALTER TABLE `plan`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `tema_contenido`
+--
+ALTER TABLE `tema_contenido`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `ventacurso`
+--
+ALTER TABLE `ventacurso`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentario`
+--
+ALTER TABLE `comentario`
+  ADD CONSTRAINT `comentario_ibfk_1` FOREIGN KEY (`Id_Usuario`) REFERENCES `usuario` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentario_ibfk_2` FOREIGN KEY (`Id_Curso`) REFERENCES `curso` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `contenido`
+--
+ALTER TABLE `contenido`
+  ADD CONSTRAINT `contenido_ibfk_1` FOREIGN KEY (`Id_Curso`) REFERENCES `curso` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `curso`
+--
+ALTER TABLE `curso`
+  ADD CONSTRAINT `curso_ibfk_1` FOREIGN KEY (`Id_Docente`) REFERENCES `docente` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `curso_ibfk_2` FOREIGN KEY (`Id_Carrera`) REFERENCES `carrera` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `tema_contenido`
+--
+ALTER TABLE `tema_contenido`
+  ADD CONSTRAINT `tema_contenido_ibfk_1` FOREIGN KEY (`Id_Curso`) REFERENCES `curso` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`Id_Curso`) REFERENCES `curso` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuario_ibfk_2` FOREIGN KEY (`Id_Plan`) REFERENCES `plan` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `usuario_ibfk_3` FOREIGN KEY (`Id_Pago`) REFERENCES `pago` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ventacurso`
+--
+ALTER TABLE `ventacurso`
+  ADD CONSTRAINT `ventacurso_ibfk_3` FOREIGN KEY (`Id_Usuario`) REFERENCES `usuario` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
