cd77c9ff7471772417348537930de2d9542d7275
José Deodanes-code <60304928+Jose-developer-start@users.noreply.github.com>
2020-10-09 10:03:36 -0600

    Add files via upload

--- config/conexion.php
+++ config/conexion.php
@@ -1,11 +1,10 @@
 <?php
-
+//PDO::MYSQL_ATTR_INIT_COMMAND-> sirve para establecer el codificado de caracteres
 try{
-	$pdo = new PDO("mysql:host=localhost;dbname=sistema-cursos;charset=utf8","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+	$pdo = new PDO("mysql:host=localhost;dbname=sistema-cursos","root","",array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
 	//echo "<script>alert('conectado')</script>";
 }catch(Exception $e){
+	die($e->getMessaget());
 	//echo "<script>alert('Error')</script>";
 }
-
-
 ?>
