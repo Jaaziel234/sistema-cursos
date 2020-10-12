81cfefcf45f6b49e0db124703e366b93412c10d2
Jaaziel234 <63311578+Jaaziel234@users.noreply.github.com>
2020-10-12 15:22:22 -0600

    Add files via upload

--- modulos/registrarse.php
+++ modulos/registrarse.php
@@ -1,7 +1,22 @@
+
+<!--a trabajar hoy domingo-->
+
 <?php
-//Recibir datos
+//Incluimos la conexion a base de datos
+include_once '../config/conexion.php';
+$nombre=$_POST['nombres'];
+	$apellido=$_POST['apellidos'];
+	$correo=$_POST['correo'];
+	$usuario=$_POST['usuario'];
+	$clave=$_POST['clave'];
+
+	$consulta=$pdo->prepare("INSERT INTO usuario(Nombres,Apellidos,Correo,Usuario,Contraseña) VALUES(?,?,?,?,?)");
 
 
-
-
+	if($consulta->execute(array($nombre,$apellido,$correo,$usuario,$clave))){
+		header('location:../index.php');
+		//echo "Datos almacenados";
+	}else{
+		//echo "Error no se pudo almacenar los datos";
+	}
 ?>
