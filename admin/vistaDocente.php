a5288a87e6e6ff0281a3526a4ea11629dcef533a
diana3b <71905518+diana3b@users.noreply.github.com>
2020-11-11 00:28:53 -0600

    Add files via upload

--- admin/vistaDocente.php
+++ admin/vistaDocente.php
@@ -14,8 +14,8 @@ include_once "templates/header.php";?>
         </div>
         <ul class="app-breadcrumb breadcrumb">
             <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-            <li class="breadcrumb-item">Tables</li>
-            <li class="breadcrumb-item active"><a href="#">Simple Tables</a></li>
+            <li class="breadcrumb-item">Docente</li>
+            <li class="breadcrumb-item active"><a href="#">Editar</a></li>
         </ul>
     </div>
     <div class="row">
