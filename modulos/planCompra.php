444dd980ea81861a52e5628bd5cf7360ce72ab69
José Deodanes-code <60304928+Jose-developer-start@users.noreply.github.com>
2020-11-12 16:47:56 -0600

    new-4

--- modulos/planCompra.php
+++ modulos/planCompra.php
@@ -1,25 +1,34 @@
 <?php
+//Iniciamos sesion para comprobar
+session_start();
+//Validando si no existe redirige a registrarse
+if(!isset($_SESSION['usuario'])){
+	header("Location:../registrarse.php");
+}
 include_once "../config/conexion.php";
+
+//Datos de compra
+$methodPago = isset($_POST['metodoPago']) ? $_POST['metodoPago'] : '';
 //ID del usuario
-$idUsuario = $_GET['idUsuario'];
+$idUsuario = $_SESSION['usuario']['Id'];
+//Consulta a la Tabla de cursos
 $selectSQL = "SELECT * FROM curso";
 $stmt = $pdo->prepare($selectSQL);
 $stmt->execute();
 $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ///Consulta para comprobar si ya existe el curso
-$selectSqlVenta = "SELECT * FROM ventacurso";
-$stmt = $pdo->prepare($selectSqlVenta);
-$stmt->execute();
-$resultVenta = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//$selectSqlVenta = "SELECT * FROM ventacurso";
+//$stmt = $pdo->prepare($selectSqlVenta);
+//$stmt->execute();
+//$resultVenta = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $sql = "INSERT INTO ventacurso (Correo,ClaveTransaccion,PaypalDato,Estado,Id_Usuario,Id_Curso) VALUES('','','','pagado',?,?)";
 $consult = $pdo->prepare($sql);
+//Fase de revision
 foreach($result as $data){
 
-		if($data['Id'] != $venta['Id_Curso'] && $idUsuario != $venta['Id_Usuario']){
-			$consult->execute(array($idUsuario,$data['Id']));
-		}
+	$consult->execute(array($idUsuario,$data['Id']));
 }
-header("Location:../index.php");
+header("Location:../misCursos.php");
 ?>
--- modulos/planes.php
+++ modulos/planes.php
@@ -1,10 +1,16 @@
 <?php
 //Conexion a BD
 include_once 'config/conexion.php';
-
+//Consulta para mostrar los planes
 $selectSQL = "SELECT * FROM plan";
 $sentencia = $pdo->prepare($selectSQL);
 $sentencia->execute();
 $resultPlanes = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+//Consulta para mostrar los tipos de pagos
+$selectSQL = "SELECT * FROM pago";
+$sentencia = $pdo->prepare($selectSQL);
+$sentencia->execute();
+$resultPago = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
